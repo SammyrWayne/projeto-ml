33e9a67e4b2d69618dd129295b3216fed7e7cab3
SammyrWayne <151514520+SammyrWayne@users.noreply.github.com>
2026-05-09 10:13:38 -0300

    Update login.py

--- login.py
+++ login.py
@@ -5,32 +5,19 @@ SENHA = "1234"
 
 def tela_login():
     
-    
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-color: #1e1e1e;
-            color: white;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    
-    )
-
     st.title("Tela do sistema")
+    
     
     usuario = st.text_input("Login:")
     senha = st.text_input("Senha: ", type="password")
 
 
 
-if st.button("Entrar"):
-    if usuario == USUARIO and senha == SENHA:
+    if st.button("Entrar"):
+        if usuario == USUARIO and senha == SENHA:
 
-        st.session_state.logado = True
-        st.rerun()
+            st.session_state.logado = True
+            st.rerun()
     
-    else:
-        st.error("Usuario ou senha invalidos!")
+        else:
+            st.error("Usuario ou senha invalidos!")
